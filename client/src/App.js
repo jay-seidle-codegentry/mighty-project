0512e60eda398a457164266327b351faccf25d13
Jay Seidle <jay@theseidles.com>
2020-04-13 21:03:35 -0400

    add login support using Auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,47 @@
 import React from 'react';
+import Main from './Components/Main';
+import Secret from './Components/Secret';
+import NotFound from './Components/NotFound';
+import CallBack from './Components/Callback';
+import { useAuth0 } from "./react-auth0-spa";
+import NavBar from './Components/NavBar';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const { isAuthenticated, loading } = useAuth0();
+
+  if (loading) {
+    return (<div>Loading...</div>);
+  }
+
+  let renderComponent = <NotFound />;
+
+  switch(props.location) {
+    case "":
+      renderComponent = <Main {...props} />;
+      break;
+    case "secret":
+      if(isAuthenticated) {
+        renderComponent = <Secret {...props} />;
+      }
+      break;
+    case "callback":
+      renderComponent = <CallBack {...props} />;
+      break;
+    default:
+      renderComponent = <NotFound />;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+  <p>{props.started}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +51,7 @@ function App() {
           Learn React
         </a>
       </header>
+      {renderComponent}
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//import Auth from './Auth';
+import { Auth0Provider } from "./react-auth0-spa";
+import config from "./auth_config.json";
+import history from "./utils/history";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+//const auth = new Auth();
+
+let state = {};
+window.setState = (changes) => {
+  state = Object.assign({}, state, changes);
+
+  ReactDOM.render(
+/*    <React.StrictMode>
+      <App {...state} />
+    </React.StrictMode>, */
+    <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App {...state} />
+  </Auth0Provider>,
+    document.getElementById('root')
+  );  
+}
+
+/* eslint no-restricted-globals: 0*/
+let initState = {
+  started: Date.now(),
+  location: location.pathname.replace(/^\/?|\/$/g, "")
+}
+
+window.setState(initState);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
